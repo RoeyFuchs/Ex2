585fef0b5e68a5ce0282672f59a21ad3f30246dc
Stav Koren <stavkoren123@gmail.com>
2019-04-23 15:00:33 +0300

    replace status model with IStatusModel

---
+++ Ex2/Models/Interface/IStatusModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex2.Models.Interface
+{
+    interface IStatusModel
+    {
+
+         string ServerColor { set; get;}
+
+         string ClientColor {set; get;}
+    }
+}
--- Ex2/Models/StatusModel.cs
+++ Ex2/Models/StatusModel.cs
@@ -1,12 +1,12 @@
-﻿using System;
+﻿using Ex2.Models.Interface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Ex2.Models {
-    class StatusModel {
-
+    class StatusModel : IStatusModel{
         public string ServerColor {
             set; get;
         }
