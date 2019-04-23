18c24eee5c4bdf43ac7063217741e26695b10cd4
Roey Fuchs <roey.fuchs@live.biu.ac.il>
2019-04-23 16:03:56 +0300

    start auto pilot

---
+++ Ex2/ViewModels/AutoPilotViewModel.cs
@@ -0,0 +1,53 @@
+﻿using Ex2.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Ex2.Views;
+using System.ComponentModel;
+
+namespace Ex2.ViewModels
+{
+    public class AutoPilotViewModel : INotifyPropertyChanged {
+        private ICommand _clearCommand;
+        private ICommand _sendCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public AutoPilotViewModel() { }
+           
+
+        public string Text { get; set; }
+
+
+        public ICommand ClearCommand {
+            get {
+                return _clearCommand ?? (_clearCommand = new CommandHandler(() => ClearTextBox()));
+            }
+        }
+        void ClearTextBox() {
+            this.Text = String.Empty;
+            PropertyChanged(this, new PropertyChangedEventArgs("Text"));
+        }
+
+
+        public ICommand SendCommand {
+            get {
+                return _sendCommand ?? (_sendCommand = new CommandHandler(() => Send()));
+            }
+        }
+        void Send() {
+            string[] commands = Text.Split(
+    new[] { Environment.NewLine },
+    StringSplitOptions.None
+);
+            foreach (var c in commands) {
+                Console.WriteLine(c);
+                Console.WriteLine("new");
+            }
+        }
+    }
+}
