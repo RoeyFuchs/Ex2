7aee1e5d268c744b864dd218af225fb608462f04
Roey Fuchs <roey.fuchs@live.biu.ac.il>
2019-04-23 16:49:25 +0300

    autopilot almost finished

--- Ex2/ViewModels/AutoPilotViewModel.cs
+++ Ex2/ViewModels/AutoPilotViewModel.cs
@@ -8,19 +8,35 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using Ex2.Views;
 using System.ComponentModel;
+using System.Threading;
 
 namespace Ex2.ViewModels
 {
     public class AutoPilotViewModel : INotifyPropertyChanged {
         private ICommand _clearCommand;
         private ICommand _sendCommand;
+        private TimeSpan interval = TimeSpan.FromSeconds(2);
+
+        const string busyColor = "#ff0000";
+        const string freeColor = "#ff00b7";
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public AutoPilotViewModel() { }
+        public AutoPilotViewModel() {
+            BackColor = freeColor;
+        }
            
 
         public string Text { get; set; }
+
+        private string _backColor;
+        public string BackColor {
+            get { return _backColor; }
+            set {
+                _backColor = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("BackColor"));
+            }
+        }
 
 
         public ICommand ClearCommand {
@@ -36,18 +52,29 @@ namespace Ex2.ViewModels
 
         public ICommand SendCommand {
             get {
-                return _sendCommand ?? (_sendCommand = new CommandHandler(() => Send()));
+                return _sendCommand ?? (_sendCommand = new CommandHandler(() => CreatSendTask()));
             }
         }
-        void Send() {
+
+        private void CreatSendTask() {
+            Task sendTask = new Task(() => {
+                Send();
+            });
+            sendTask.Start();
+        }
+
+
+        private void Send() {
+            BackColor = busyColor;
             string[] commands = Text.Split(
-    new[] { Environment.NewLine },
-    StringSplitOptions.None
-);
+                                new[] { Environment.NewLine },
+                                StringSplitOptions.None
+                                );
             foreach (var c in commands) {
-                Console.WriteLine(c);
-                Console.WriteLine("new");
+                Client.Instance.addCommand(c, true);
+                Thread.Sleep(interval);
             }
+            BackColor = freeColor;
         }
     }
 }
